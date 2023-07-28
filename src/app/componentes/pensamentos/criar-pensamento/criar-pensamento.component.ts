93247a432d05904d8dcaa450d4e23eaf02ee0c0b
Rebecca Moraes <rgabrielle@ciandt.com>
2023-07-27 22:13:58 -0300

    Parte 1 do curso da alura - conceitos básicos e CRUD

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,40 @@
+import { PensamentoService } from './../pensamento.service';
+import { Pensamento } from './../pensamento/pensamento';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent implements OnInit {
+
+  formulario!: FormGroup;
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo: ['Formulário reativo'],
+      autoria: ['Angular'],
+      modelo: ['modelo2']
+    });
+  }
+
+  criarPensamento() {
+    this.service.criar(this.formulario.value).subscribe(() => {
+      this.router.navigate(['/listarPensamento']);
+    });
+  }
+
+  cancelarPensamento() {
+    this.router.navigate(['/listarPensamento']);
+  }
+
+}
---
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -0,0 +1,40 @@
+import { Pensamento } from './pensamento/pensamento';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PensamentoService {
+
+  private readonly API_URL = "http://localhost:3000/pensamentos";
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  listar(): Observable<Pensamento[]> {
+    return this.http.get<Pensamento[]>(this.API_URL);
+  }
+
+  criar(pensamento: Pensamento): Observable<Pensamento> {
+    return this.http.post<Pensamento>(this.API_URL, pensamento);
+  }
+
+  editar(pensamento: Pensamento): Observable<Pensamento> {
+    const url = `${this.API_URL}/${pensamento.id}`;
+    return this.http.put<Pensamento>(url, pensamento);
+  }
+
+  excluir(id: number):Observable<Pensamento> {
+    const url = `${this.API_URL}/${id}`;
+    return this.http.delete<Pensamento>(url);
+  }
+
+  buscarPorId(id: number): Observable<Pensamento> {
+    const url = `${this.API_URL}/${id}`;
+    return this.http.get<Pensamento>(url);
+  }
+
+}
