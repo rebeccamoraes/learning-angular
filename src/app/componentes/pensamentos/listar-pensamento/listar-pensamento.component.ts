90fe1b86a9d3c6746d1f6d51658b723f2806a837
Rebecca Moraes <rgabrielle@ciandt.com>
2023-07-28 08:08:41 -0300

    Implementando paginação

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -10,13 +10,26 @@ import { Component, OnInit } from '@angular/core';
 export class ListarPensamentoComponent implements OnInit {
   listaPensamentos: Pensamento[] = [];
 
+  paginaAtual = 1;
+  haMaisPensamentos: boolean = true;
+
   constructor(
     private service: PensamentoService
   ) { }
 
   ngOnInit(): void {
-    this.service.listar().subscribe(listaPensamentos => {
+    this.service.listar(this.paginaAtual).subscribe(listaPensamentos => {
       this.listaPensamentos = listaPensamentos;
+    });
+  }
+
+  carregarMaisPensamentos() {
+    this.service.listar(++this.paginaAtual)
+    .subscribe(listaPensamentos => {
+      this.listaPensamentos.push(...listaPensamentos);
+      if(!listaPensamentos.length) {
+        this.haMaisPensamentos = false;
+      }
     });
   }
 
--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,6 +1,6 @@
 import { Pensamento } from './pensamento/pensamento';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -14,8 +14,14 @@ export class PensamentoService {
     private http: HttpClient
   ) { }
 
-  listar(): Observable<Pensamento[]> {
-    return this.http.get<Pensamento[]>(this.API_URL);
+  listar(pagina: number): Observable<Pensamento[]> {
+    const itensPorPagina = 6;
+
+    let params = new HttpParams()
+      .set('_page', pagina)
+      .set("_limit", itensPorPagina);
+
+    return this.http.get<Pensamento[]>(this.API_URL, { params });
   }
 
   criar(pensamento: Pensamento): Observable<Pensamento> {
